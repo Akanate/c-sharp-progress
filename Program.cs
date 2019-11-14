a1efb53d4cf07cc1fe6fb6402d6020d4618c5ee1
WHYSOEASY <48154623+WHYSOEASY@users.noreply.github.com>
2019-11-14 19:19:55 +0000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,28 +1,26 @@
-﻿using System;
+using System;
 using System.IO;
-namespace Program
+namespace new_program
 {
     class Program
     {
-        public void Main_menu()
+        public static void NewMenu()
         {
             Console.WriteLine("Welcome to the password checker");
             Console.WriteLine("1.To Check your password");
             Console.WriteLine("2.To exit");
-            var classed = new Program();
             if (Console.ReadLine() == "1")
             {
-                classed.password_check();
+                Passwordcheck();
             }
             else
             {
                 Environment.Exit(0);
             }
         }
-        public void password_check()
+        public static void Passwordcheck()
         {
             string password1 = "";
-            var classes = new Program();
             Console.Write("Enter the password you want to check: ");
             password1 = Console.ReadLine();
             Console.WriteLine("You put {0}", password1);
@@ -35,20 +33,20 @@ namespace Program
             {
                 if (lineRead == password1)
                 {
-                    Console.WriteLine("Your password is {0} the amount of passwords it took to find yours was {1}", lineRead,counter);
-                    classes.password_check();
-                } else
+                    Console.WriteLine("Your password is {0} the amount of passwords it took to find yours was {1}", lineRead, counter);
+                    Passwordcheck();
+                }
+                else
                 {
                     counter += 1;
                     continue;
                 }
             }
         }
-
-        private static void Main(string[] args)
+        
+        void Main(string[] args)
         {
-            var class1 = new Program();
-            class1.Main_menu();
+            NewMenu();
         }
 
     };
